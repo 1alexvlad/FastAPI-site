a9d2a55152dbfa4255dd7c36aff0567c945298a2
Alexandrov Vladislav <zxcqwe1954@mail.ru>
2024-09-30 21:17:41 +0300

    версионирование

--- bookings/router.py
+++ bookings/router.py
@@ -2,6 +2,8 @@ from datetime import date
 from typing import List
 
 from fastapi import APIRouter, Depends
+from fastapi_versioning import version
+
 
 from bookings.schema import SBookings
 from bookings.service import BookingService
@@ -15,11 +17,13 @@ router = APIRouter(
 )
 
 @router.get('')
+@version(1)
 async def get_bookings(user: Users = Depends(get_current_user)) -> List[SBookings]:
     return await BookingService.find_all(user_id=user.id)
 
 
 @router.post('')
+@version(1)
 async def add_booking(room_id: int, date_from: date, date_to: date, 
                       user: Users = Depends(get_current_user)) -> SBookings:
     booking = await BookingService.add(user.id, room_id, date_from, date_to)
@@ -29,5 +33,6 @@ async def add_booking(room_id: int, date_from: date, date_to: date,
 
 
 @router.delete('/{booking_id}')
+@version(1)
 async def delete_booking(booking_id: int, user: Users = Depends(get_current_user)):
     await BookingService.delete(booking_id, user.id)
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
+from fastapi_versioning import VersionedFastAPI
 from sqladmin import Admin
-from database import engine
-from admin.views import BookingsAdmin, HotelsAdmin, RoomsAdmin, UserAdmin
-from admin.auth import authentication_backend
 
-from hotels.router import router as router_hotels
-from users.router import auth_router
-from users.router import user_router
+from admin.auth import authentication_backend
+from admin.views import BookingsAdmin, HotelsAdmin, RoomsAdmin, UserAdmin
 from bookings.router import router as router_bookings
+from database import engine
+from hotels.router import router as router_hotels
 from room.router import router as router_room
+from users.router import auth_router, user_router
 
 app = FastAPI()
 
@@ -19,6 +19,16 @@ app.include_router(router_bookings)
 app.include_router(router_room)
 
 
+app = VersionedFastAPI(app,
+    version_format='{major}',
+    prefix_format='/v{major}',
+    # description='Greet users with a nice message',
+    # middleware=[
+    #     Middleware(SessionMiddleware, secret_key='mysecretkey')
+    # ]
+)
+
+
 admin = Admin(app, engine, authentication_backend=authentication_backend)
 
 
@@ -26,3 +36,4 @@ admin.add_view(UserAdmin)
 admin.add_view(BookingsAdmin)
 admin.add_view(HotelsAdmin)
 admin.add_view(RoomsAdmin)
+
